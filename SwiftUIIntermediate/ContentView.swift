9724829aa9b58542a1fd77fd514a77c441b71218
Shahbaz Alam <shahbazjamia1957@gmail.com>
2025-12-18 12:41:11 +0530

    Initial Commit

---
+++ SwiftUIIntermediate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIIntermediate
+//
+//  Created by SHAHBAZ ALAM on 18/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIIntermediate/SwiftUIIntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIntermediateApp.swift
+//  SwiftUIIntermediate
+//
+//  Created by SHAHBAZ ALAM on 18/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
