17a1b971a8a0edd8a15b144d16fc3c9154f0bd8a
Shahbaz Alam <shahbazjamia1957@gmail.com>
2025-12-18 16:31:21 +0530

    add Sheets for Pop-up

---
+++ SwiftUIIntermediate/PopUpSheets.swift
@@ -0,0 +1,18 @@
+//
+//  PopUpSheets.swift
+//  SwiftUIIntermediate
+//
+//  Created by SHAHBAZ ALAM on 18/12/25.
+//
+
+import SwiftUI
+
+struct PopUpSheets: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopUpSheets()
+}
