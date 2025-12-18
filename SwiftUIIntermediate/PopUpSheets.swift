12948e37446f1388da7a1c5b21a737fa261893f2
Shahbaz Alam <shahbazjamia1957@gmail.com>
2025-12-18 16:31:48 +0530

    add Sheets For Pop-up

--- SwiftUIIntermediate/PopUpSheets.swift
+++ SwiftUIIntermediate/PopUpSheets.swift
@@ -1,18 +1,56 @@
-//
-//  PopUpSheets.swift
-//  SwiftUIIntermediate
-//
-//  Created by SHAHBAZ ALAM on 18/12/25.
-//
-
 import SwiftUI
 
 struct PopUpSheets: View {
+    
+    @State private var showSheet: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.green
+                .ignoresSafeArea()
+            
+            Button {
+                showSheet.toggle()
+            } label: {
+                Text("Open Sheet")
+                    .font(.headline)
+                    .padding()
+                    .foregroundColor(.red)
+                    .background(Color.white)
+                    .cornerRadius(10)
+            }
+            .sheet(isPresented: $showSheet) {
+                SecondScreen()
+            }
+        }
+    }
+}
+
+struct SecondScreen: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Color.gray
+                .ignoresSafeArea()
+            
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "xmark")
+                    .font(.title)
+                    .padding()
+                    .background(Color.red)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                
+            }
+        }
     }
 }
 
 #Preview {
     PopUpSheets()
 }
+
